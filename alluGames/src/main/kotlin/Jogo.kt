97e635a73d96ee95946558692359a7472a2b5a22
SantosRodrigo89 <rodrigo.v.santios89@gmail.com>
2024-02-17 19:22:42 -0300

    feat: criando a classe jogo

---
+++ alluGames/src/main/kotlin/Jogo.kt
@@ -0,0 +1,12 @@
+class Jogo {
+    var titulo = ""
+    var capa = ""
+    val descricao = ""
+
+    override fun toString(): String {
+        return "Meu Jogo: \n" +
+                "Título: $titulo \n" +
+                "Capa: $capa \n" +
+                "Descricao: $descricao"
+    }
+}
