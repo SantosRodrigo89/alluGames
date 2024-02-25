fb179502b8ec45e49754c1b60f21bb50f9208b0c
SantosRodrigo89 <rodrigo.v.santos89@gmail.com>
2024-02-25 16:14:37 -0300

    feat: add novos metodos

--- alluGames/src/main/kotlin/br/com/alura/allugames/modelo/Gamer.kt
+++ alluGames/src/main/kotlin/br/com/alura/allugames/modelo/Gamer.kt
@@ -1,5 +1,7 @@
 package br.com.alura.allugames.modelo
 
+import kotlin.random.Random
+
 data class Gamer(var nome:String, var email:String) {
     var dataNascimento:String? = null
     var usuario:String? = null
@@ -9,9 +11,17 @@ data class Gamer(var nome:String, var email:String) {
         this(nome, email) {
             this.dataNascimento = dataNascimento
             this.usuario = usuario
+            criarIdInterno()
         }
 
     override fun toString(): String {
         return "Gamer(nome='$nome', email='$email', dataNascimento=$dataNascimento, usuario=$usuario, idInterno=$idInterno)"
+    }
+
+    fun criarIdInterno() {
+        val numero = Random.nextInt(10000)
+        val tag = String.format("%04d", numero)
+
+      idInterno = "$usuario#$tag"
     }
 }
